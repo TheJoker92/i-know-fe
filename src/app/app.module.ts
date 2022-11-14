1d5bdd2b45b4a804a8b12ef336fe80ccd6bd4178
TheJoker92 <demicco.alessandro92@gmail.com>
2022-11-14 20:13:04 +0100

    lock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { WebcamModule } from 'ngx-webcam';
 import { SpeechSynthesisModule } from '@kamiazya/ngx-speech-synthesis';
 import { AppComponent } from './app.component';
 import { LabelComponent } from './label/label.component';
@@ -23,7 +22,6 @@ import { Model3dComponent } from './model3d/model3d.component';
   ],
   imports: [
     BrowserModule,
-    WebcamModule,
     SpeechSynthesisModule.forRoot({
       lang: 'en',
       volume: 1.0,
