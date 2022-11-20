59a683397c422175cd046e1b0aa1a7a9a0fcb86e
TheJoker92 <demicco.alessandro92@gmail.com>
2022-11-20 23:18:14 +0100

    path models

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 
 const app = express();
 
-console.log(__dirname)
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/i-know-fe'));
 
